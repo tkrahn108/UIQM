6764f80d5945cc1d0e4fb7e36710322ab813dc72
Thomas Krahn <tkrahn@techfak.uni-bielefeld.de>
2016-07-04 14:59:59 +0200

    First version for UIConM

--- src/uiconm.h
+++ src/uiconm.h
@@ -5,10 +5,16 @@
 
 using namespace cv;
 
-class Uiconm{
+class Uiconm {
 public:
-    float calculate (Mat img);    
-    
+    float calculate(Mat img);
+
+private:
+    float plipG(float intensity);
+    float plipAddition(float g1, float g2);
+    float plipSubtraction(float g1, float g2);
+    float plipMultiplication(float c, float g);
+    void findMinMaxIntensity(Mat img, int xmin, int xmax, int ymin, int ymax, int& min, int& max);
 };
 
 
